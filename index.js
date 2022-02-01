a49f2b6a6a672c3b37f8d294a1a2a863e54fe3c3
Evan Chernicky <echernicky@gmail.com>
2022-02-01 00:40:40 -0500

    commit tree 89ed63447159154ccf2e66c1b05f350b068569c1

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(distance) {
+
+    return Math.abs(42 - distance);
+}
+distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(blocks) {
+
+   const numOfBlocks = distanceFromHqInBlocks(blocks);
+   return numOfBlocks * 264;
+
+}
+distanceFromHqInFeet(50);
+
+function distanceTravelledInFeet(start, destination) {
+
+    return 264 * Math.abs(start - destination);
+
+}
+distanceTravelledInFeet(43, 42);
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    const feetWithDiscount = distanceInFeet - 400;
+
+    if (distanceInFeet < 400) {
+        return 0;
+    }
+    else if (distanceInFeet > 400 && distanceInFeet < 2000) {
+        return feetWithDiscount * 0.02;
+    }
+    else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+        return 25;
+    }
+    else {
+        return "cannot travel that far";
+    }
+}
+calculatesFarePrice(42,38);
